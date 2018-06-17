c0c7b7fa62f85466d11a379a1fd56d735c8785bd
holycrab13 <jan.forberg@hotmail.de>
2018-06-17 21:43:04 +0200

    Update footer.php

--- code/php/viewer/footer.php
+++ code/php/viewer/footer.php
@@ -3,7 +3,7 @@
             <div class="container">
                 <div class="content has-text-centered">
                     <p>
-                        <strong>WebId.</strong> Authentication made simple.
+                        <strong>Congratulations!</strong> You have reached the footer.
                     </p>
 
                 </div>
