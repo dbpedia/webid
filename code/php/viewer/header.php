0f9e3095bec4b4ff600cb55226b3e40a36be2336
holycrab13 <jan.forberg@hotmail.de>
2018-06-17 19:41:41 +0200

    Create header.php

---
+++ code/php/viewer/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+    <head>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+        <title>Membership Website Sample</title>
+        <link rel="stylesheet" href="bulma/css/bulma.css"/>
+        <link rel="stylesheet" href="webid.css"/>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="js/general.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    </head>
+    <body>
