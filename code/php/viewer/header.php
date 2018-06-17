d13d6d9b8b57dff771584e46b2ebb205bee5bfad
holycrab13 <jan.forberg@hotmail.de>
2018-06-17 21:43:25 +0200

    Update header.php

--- code/php/viewer/header.php
+++ code/php/viewer/header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
     <head>
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-        <title>Membership Website Sample</title>
+        <title>WebId Messages</title>
         <link rel="stylesheet" href="bulma/css/bulma.css"/>
         <link rel="stylesheet" href="webid.css"/>
 
