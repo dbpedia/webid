825173080f4a4869a1657446fda5c09bed4b422d
holycrab13 <jan.forberg@hotmail.de>
2018-06-17 19:42:03 +0200

    Create footer.php

---
+++ code/php/viewer/footer.php
@@ -0,0 +1,14 @@
+				
+				<footer class="footer">
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p>
+                        <strong>WebId.</strong> Authentication made simple.
+                    </p>
+
+                </div>
+            </div>
+        </footer>
+    </body>
+
+</html>
