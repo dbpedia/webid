5037f56beaed7d6ad1b7ddde325677e4bf267157
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2018-08-02 11:15:54 +0200

    error codes

--- code/php/lib/server-webidauth/error_codes.php
+++ code/php/lib/server-webidauth/error_codes.php
@@ -5,9 +5,22 @@ class StatusCode
 	
 	const AUTH_SUCCESS = 0;
 
-	// Starting with 2x invalid certificate
-	//const INVALID_CERTIFICATE = 2;
+	// 2x invalid certificate
+	// const INVALID_CERTIFICATE = 2;
 	const CERT_NOT_PASSED = 21;
+	//21 cert malformed, not readable
+	//23 SAN field not set
+	//24 malformed url in SAN
+	
+	// 3x WebId
+	const WEBID_NOT_LOADABLE = 31
+	// 32 not parseable + parse error
+	// 33 no key found
+	// 34 bad key
+	
+	// 4x Auth error
+	// 41 keys don't match
+
 
 	const msg = array (
 		StatusCode::CERT_NOT_PASSED => "[".StatusCode::CERT_NOT_PASSED."] Client has not passed a certificate\n"
@@ -15,5 +28,5 @@ class StatusCode
 	
 }
 
-echo StatusCode::msg[StatusCode::CERT_NOT_PASSED];
+//echo StatusCode::msg[StatusCode::CERT_NOT_PASSED];
 
